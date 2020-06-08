6ef9d2f82c0fa527445db001c88315026922b27b
Andrew Blignaut <andrew.blignaut@statpro.com>
2020-06-08 13:39:41 +0200

    rails g scaffold Ticket url:string

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  resources :tickets
   root to: 'visitors#index'
 end
---
+++ spec/factories/tickets.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :ticket do
+    url { "MyString" }
+  end
+end
